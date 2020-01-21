52f7de333e49fc7a450f104f1a73505d39bedc63
kpotapovich <kris.potapovich@gmail.com>
2020-01-21 14:12:07 +0300

    task 2 подключила js, media.css, адаптивная верстка, делала гамбургер.

---
+++ lesson_02/js.js
@@ -0,0 +1,15 @@
+var burger = document.getElementById("burger");
+var menu = document.getElementById("menu");
+var bg = document.getElementById("bg");
+burger.addEventListener("click", function () {
+    var isCross = burger.classList.contains("cross");
+    if (isCross){
+        burger.classList.remove("cross");
+        menu.classList.remove("visible");
+        bg.classList.remove("visible");
+} else {
+        burger.classList.add("cross");
+        menu.classList.add("visible");
+        bg.classList.add("visible");
+    }
+})
