7c5971c0fe6395dbac37f87da0eb6742b0696b4a
kpotapovich <kris.potapovich@gmail.com>
2020-02-05 16:20:44 +0300

    lesson_02  Дз: создание переменных, объектов и массивов.

--- lesson_02/main.js
+++ lesson_02/main.js
@@ -20,3 +20,9 @@ $('.lead_cot').slick({
     arrows: false,
 
 });
+var profile = {name:"katia", surname:"chitnik", age: 24, faculty:"journalism"};
+var template = {name:"kris", surname:"potapovich", age: 22, favoriteColor:"red"};
+var passport = {name:"andrei", surname:"naruts", age: 20, residence:"minsk"};
+var certificate = {name:"lusha", surname:"prokhorenko", patronymic:"Alexandrovich", age: 29,};
+var data = [profile, template, passport, certificate];
+console.log (data);
