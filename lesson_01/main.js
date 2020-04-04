0786a55cb7706fc11a126b2bdbc68b075f03ae07
kpotapovich <kris.potapovich@gmail.com>
2020-04-04 12:15:28 +0300

    сайт макет

---
+++ lesson_01/main.js
@@ -0,0 +1,30 @@
+var burger = document.getElementById("burger");
+var menu = document.getElementById("menu");
+var bg = document.getElementById("bg");
+burger.addEventListener("click", function () {
+    var isCross = burger.classList.contains("cross");
+    if (isCross){
+        burger.classList.remove("cross");
+        menu.classList.remove("visible");
+        bg.classList.remove("visible");
+} else {
+        burger.classList.add("cross");
+        menu.classList.add("visible");
+        bg.classList.add("visible");
+    }
+});
+$('.lead_cot').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+
+});
+
+// var profile = {name:"katia", surname:"chitnik", age: 24, faculty:"journalism"};
+// var template = {name:"kris", surname:"potapovich", age: 22, favoriteColor:"red"};
+// var passport = {name:"andrei", surname:"naruts", age: 20, residence:"minsk"};
+// var certificate = {name:"lusha", surname:"prokhorenko", patronymic:"Alexandrovich", age: 29,};
+// var data = [profile, template, passport, certificate];
+// console.log (data);
+
